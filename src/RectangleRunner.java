ab13d041568fd3070c909e085eeae03a49612a68
BT_2N2_19 <sample@gmail.com>
2023-09-22 13:35:17 -0400

    Added another method

--- src/RectangleRunner.java
+++ src/RectangleRunner.java
@@ -4,9 +4,14 @@ public class RectangleRunner {
         // creating one instance/object from the Rectangle class
         Rectangle rect1 = new Rectangle(5, 6);
         rect1.printArea();  // calling a method on the object
+        rect1.printPerimeter();
 
         // creating ANOTHER instance/object from the Rectangle class
         Rectangle rect2 = new Rectangle(10, 8);
         rect2.printArea();  // calling a method on the object
+        rect2.printPerimeter();
+        Rectangle rect3 = new Rectangle(15,7);
+        rect3.printArea();
+        rect3.printPerimeter();
     }
 }
